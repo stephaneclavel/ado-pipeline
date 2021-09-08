7eb535079a51095b69a7ae0307bd67c10dea79ee
Stephane Clavel <stef@decidela.net>
2021-09-08 18:16:02 +0200

    removed pipeline kv policy

--- 2-adding-sp-creation-and-kv/setup/keyvault.tf
+++ 2-adding-sp-creation-and-kv/setup/keyvault.tf
@@ -14,7 +14,7 @@ resource "azurerm_key_vault" "setup" {
 
 # Set access policies
 # Grant yourself full access (probably could be restricted to just secret_permissions)
-resource "azurerm_key_vault_access_policy" "manual-terraform" {
+resource "azurerm_key_vault_access_policy" "kv-policy" {
   key_vault_id = azurerm_key_vault.setup.id
 
   tenant_id = data.azurerm_client_config.current.tenant_id
@@ -33,23 +33,10 @@ resource "azurerm_key_vault_access_policy" "manual-terraform" {
   #  ]
 }
 
-# Grant the pipeline SP access to [get,list] secrets from the KV
-resource "azurerm_key_vault_access_policy" "pipeline" {
-  key_vault_id = azurerm_key_vault.setup.id
-
-  tenant_id = data.azurerm_client_config.current.tenant_id
-  object_id = azuread_service_principal.service_connection.object_id
-
-  secret_permissions = [
-    "get", "list",
-  ]
-
-}
-
 # Populate with secrets to be used by the pipeline
 resource "azurerm_key_vault_secret" "pipeline" {
   depends_on = [
-    azurerm_key_vault_access_policy.manual-terraform
+    azurerm_key_vault_access_policy.kv-policy
   ]
   for_each     = local.pipeline_variables
   name         = each.key
