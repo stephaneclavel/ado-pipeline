4117832382cee88f5c80745440515b62584d0c97
steph <github@decidela.net>
2021-09-22 17:34:58 +0200

    terra fmt

--- 3-adding-checkov/vnet/main.tf
+++ 3-adding-checkov/vnet/main.tf
@@ -11,7 +11,7 @@ terraform {
     }
   }
 
-  backend "azurerm" {    
+  backend "azurerm" {
   }
 }
 
@@ -66,7 +66,7 @@ module "network" {
   resource_group_name = azurerm_resource_group.vnet.name
   vnet_name           = local.name
   address_space       = "10.0.0.0/16"
-  subnet_prefixes     = ["10.0.0.0/24","10.0.2.0/24","10.0.3.0/24"]
+  subnet_prefixes     = ["10.0.0.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
 
   depends_on = [azurerm_resource_group.vnet]
@@ -74,19 +74,19 @@ module "network" {
 
 
 resource "azurerm_network_security_group" "allow_ssh" {
-  name = "allow_ssh"
-  location = var.region
+  name                = "allow_ssh"
+  location            = var.region
   resource_group_name = azurerm_resource_group.vnet.name
 
   security_rule {
-    name = "allow_ssh"
-    priority = 100
-    direction = "Inbound"
-    access = "Deny"
-    protocol = "Tcp"
-    source_port_range = "*"
-    source_address_prefix = "*"
-    destination_port_range = "22"
+    name                       = "allow_ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    source_address_prefix      = "*"
+    destination_port_range     = "22"
     destination_address_prefix = "*"
   }
 }
